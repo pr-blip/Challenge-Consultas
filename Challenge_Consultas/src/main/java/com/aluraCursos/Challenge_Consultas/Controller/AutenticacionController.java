7fbb5dbe21982c2997add3431505045db13a8893
Meyling Rojas <mpatric03@hotmail.com>
2025-08-16 20:05:28 -0600

    Corrige flujo de login en el controlador y servicio de autenticación

--- Challenge_Consultas/src/main/java/com/aluraCursos/Challenge_Consultas/Controller/AutenticacionController.java
+++ Challenge_Consultas/src/main/java/com/aluraCursos/Challenge_Consultas/Controller/AutenticacionController.java
@@ -1,4 +1,38 @@
 package com.aluraCursos.Challenge_Consultas.Controller;
 
+import com.aluraCursos.Challenge_Consultas.domain.Usuario.DatosAutenticacion;
+import com.aluraCursos.Challenge_Consultas.domain.Usuario.Usuario;
+import com.aluraCursos.Challenge_Consultas.infra.security.DatosTokenJWT;
+import com.aluraCursos.Challenge_Consultas.infra.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class AutenticacionController {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datos){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
+        var autenticacion = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+
+        return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
+
+    }
+
 }
--- Challenge_Consultas/src/main/java/com/aluraCursos/Challenge_Consultas/domain/Usuario/AutenticacionService.java
+++ Challenge_Consultas/src/main/java/com/aluraCursos/Challenge_Consultas/domain/Usuario/AutenticacionService.java
@@ -1,4 +1,19 @@
 package com.aluraCursos.Challenge_Consultas.domain.Usuario;
 
-public class AutenticacionService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
 }
